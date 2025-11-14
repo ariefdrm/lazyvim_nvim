b91e63f10f98b76f5ea1c0774bda879d869fe3bf
ariefdrm <ariefdarmawan5522@gmail.com>
2025-11-14 11:15:33 +0700

    feat: adding tmux integration with neovim

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -39,24 +39,4 @@ return {
 
   -- which key
   { "folke/which-key.nvim", enabled = false },
-
-  -- tmux navigator
-  {
-    "christoomey/vim-tmux-navigator",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-      "TmuxNavigatorProcessList",
-    },
-    keys = {
-      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-    },
-  },
 }
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -53,4 +53,25 @@ return {
     build = ":Cord update",
     -- opts = {}
   },
+
+  -- tmux navigator
+  {
+    "christoomey/vim-tmux-navigator",
+    event = "BufReadPost",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
 }
