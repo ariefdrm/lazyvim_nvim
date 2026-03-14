c00ef26b3e56d1236feb2059791cd543c477a891
ariefdrm <ariefdarmawan5522@gmail.com>
2026-03-14 20:50:56 +0700

    feat: adding lazygit
    adding lazygit plugin into neovim config file

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -13,8 +13,33 @@ return {
     end,
   },
 
+  -- snacks nvim
+  {
+    "folke/snacks.nvim",
+    priority = 1010,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      bigfile = { enabled = true },
+      dashboard = { enabled = true },
+      explorer = { enabled = false },
+      indent = { enabled = true },
+      input = { enabled = true },
+      picker = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+    },
+  },
+
   -- Configure LazyVim to load gruvbox
-  { "LazyVim/LazyVim", opts = { colorscheme = "tokyonight" } },
+  { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin" } },
 
   -- neotree file explorer
   {
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -6,20 +6,21 @@ return {
   { "ellisonleao/gruvbox.nvim", priority = 1000, enabled = false },
 
   -- catppuccin
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000, lazy = false, enabled = false },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000, lazy = false, enabled = true },
 
   -- rosepine
-  { "rose-pine/neovim", name = "rose-pine", lazy = false, enabled = false },
+  { "rose-pine/neovim", name = "rose-pine", priority = 1000, lazy = false, enabled = false },
 
   -- tokyonight
-  { "folke/tokyonight.nvim", enabled = true, opts = { transparent = true } },
+  { "folke/tokyonight.nvim", priority = 1000, enabled = false, opts = { transparent = true } },
 
   -- nightfox
-  { "EdenEast/nightfox.nvim", enabled = false }, -- lazy
+  { "EdenEast/nightfox.nvim", priority = 1000, enabled = true }, -- lazy
 
   -- toggleterm
   {
     "akinsho/toggleterm.nvim",
+    event = "BufReadPost",
     version = "*",
     config = function()
       require("plugins.configs.toggleterm_conf")
@@ -57,14 +58,38 @@ return {
     opts = require("plugins.configs.comment_conf"),
   },
 
+  -- lazygit
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+
   -- transparent nvim
   {
     "xiyaowong/transparent.nvim",
+    enabled = false,
     event = "VimEnter",
     config = function()
       require("transparent").clear_prefix("NeoTree")
       require("transparent").clear_prefix("Lualine")
       require("transparent").clear_prefix("BufferLine")
+      require("transparent").clear_prefix("telescope")
     end,
   },
 }
